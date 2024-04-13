c27228e2762e104ead135bda5f9bc8c1c3308dc2
Minori Otsuka <lagoon_impetus.0d@icloud.com>
2024-04-13 10:42:10 +0900

    4/13

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAtMoTVJduyzaV0VOpT_UTwzftAKms0Tg4",
-    authDomain: "ocha-cat.firebaseapp.com",
-    projectId: "ocha-cat",
-    storageBucket: "ocha-cat.appspot.com",
-    messagingSenderId: "357673253800",
-    appId: "1:357673253800:web:e40dc6848889ad9f95bb9d"
+  apiKey: "AIzaSyAK1tkQo1NZsvBcHqpX2BnggcoG4xE7Uh4",
+  authDomain: "ocha-cat-dc7f8.firebaseapp.com",
+  projectId: "ocha-cat-dc7f8",
+  storageBucket: "ocha-cat-dc7f8.appspot.com",
+  messagingSenderId: "592316673962",
+  appId: "1:592316673962:web:2ced5343b43c46709945d3",
+  measurementId: "G-JGP53F7DYL"
 };
 
-// Firebaseの初期化
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
 
-   // ログイン画面の構成
-   let uiConfig = {
-    callbacks: {
-        signInSuccessWithAuthResult: function () {
-            console.log("ログイン成功")
-            return true;
-        },
-    },
-    signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-};
+const analytics = getAnalytics(app);
 
-let ui = new firebaseui.auth.AuthUI(firebase.auth());
-// ログインUIを表示
-ui.start('#firebaseui-auth-container', uiConfig);
+//新規登録処理
+register.addEventListener('click', function(e) {
+    var mailAddress = document.getElementById('mailAddress').value;
+    var password = document.getElementById('password').value;
+    
+    firebase.auth().createUserWithEmailAndPassword(mailAddress, password)
+    .catch(function(error) {
+      alert('登録できません（' + error.message + '）');
+    });
+  });
 
-// Firebase認証の初期化
-const auth = firebase.auth();
-// // Firebase UIの初期化
-// const ui = new firebaseui.auth.AuthUI(auth);
- 
+  //ログイン処理
+login.addEventListener('click', function(e) {
+    var mailAddress = document.getElementById('mailAddress').value;
+    var password = document.getElementById('password').value;
+    
+    firebase.auth().signInWithEmailAndPassword(mailAddress, password)
+    .catch(function(error) {
+      alert('ログインできません（' + error.message + '）');
+    });
+  });
+
+  //認証状態の確認
+firebase.auth().onAuthStateChanged(function(user) {
+    if(user) {
+      //ログイン状態
+      alert("ログインに成功しました");
+    }else{
+      //ログアウト状態
+    }
+  });
